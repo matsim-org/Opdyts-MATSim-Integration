fe80f5378bca9ccc5eed7e760d30c0896a9a1f68
GunnarF <GunnarF@GUNNARF-830G5.vti.se>
2018-11-11 11:57:44 +0100

    minor

--- src/main/java/org/matsim/contrib/opdyts/buildingblocks/calibration/counting/Filter.java
+++ src/main/java/org/matsim/contrib/opdyts/buildingblocks/calibration/counting/Filter.java
@@ -26,6 +26,15 @@ package org.matsim.contrib.opdyts.buildingblocks.calibration.counting;
  */
 public interface Filter<T> {
 
+	public static <T> Filter<T> newAcceptAllFilter(Class<T> clazz) {
+		return new Filter<T>() {
+			@Override
+			public boolean test(T object) {
+				return true;
+			}
+		};
+	}
+
 	public static <T> Filter<T> newSingleObjectFilter(final T targetObject) {
 		if (targetObject == null) {
 			throw new RuntimeException("Target object must not be null.");
